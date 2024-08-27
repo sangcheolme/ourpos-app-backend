ae9d44dae913831194bb118cba72ef155fa67d00
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-08-27 15:16:38 +0900

    init: ourpos-app 초기 세팅

---
+++ src/main/java/com/ourposapp/OurposAppApplication.java
@@ -0,0 +1,13 @@
+package com.ourposapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurposAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurposAppApplication.class, args);
+    }
+
+}
