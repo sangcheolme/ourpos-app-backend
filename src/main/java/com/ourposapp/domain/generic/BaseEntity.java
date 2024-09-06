4750f88e59f85a8edf12d7c3552cdfda806f21a3
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-09-06 17:12:12 +0900

    feat: BaseEntity, BaseTimeEntity 구현

---
+++ src/main/java/com/ourposapp/domain/generic/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.ourposapp.domain.generic;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
