6ecab9d4dbdeeace05cada6fc3b4246c3d2651a8
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-09-14 18:25:10 +0900

    feat: FeignConfiguration 설정

---
+++ src/main/java/com/ourposapp/global/error/FeignClientExceptionErrorDecoder.java
@@ -0,0 +1,35 @@
+package com.ourposapp.global.error;
+
+import org.springframework.http.HttpStatus;
+
+import feign.FeignException;
+import feign.Response;
+import feign.RetryableException;
+import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FeignClientExceptionErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder errorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        log.error("{} 요청 실패, status: {}, reason: {},", methodKey, response.status(), response.reason());
+
+        FeignException exception = FeignException.errorStatus(methodKey, response);
+        HttpStatus httpStatus = HttpStatus.valueOf(response.status());
+        if (httpStatus.is5xxServerError()) {
+            return new RetryableException(
+                response.status(),
+                exception.getMessage(),
+                response.request().httpMethod(),
+                exception,
+                (Long)null,
+                response.request()
+            );
+        }
+
+        return errorDecoder.decode(methodKey, response);
+    }
+}
