57b2978281501989eb1798d48cda0d757007ad40
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-09-17 23:56:19 +0900

    feat: token manager 빈 등록 및 스웨거 설정

---
+++ src/main/java/com/ourposapp/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,63 @@
+package com.ourposapp.global.config.swagger;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ourposapp.global.resolver.login.Login;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@OpenAPI30
+@OpenAPIDefinition(
+    info = @Info(
+        title = "OURPOS",
+        version = "1.0.0",
+        description = "OURPOS 주문 관리 시스템 API"
+    )
+)
+@Configuration
+public class SwaggerConfig {
+
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(Login.class);
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+            .group("OURPOS-APP")
+            .packagesToScan("com.ourposapp.api")
+            .pathsToMatch("/api/v1/**")
+            .pathsToExclude("/api/v1/admin/**")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi groupedOpenAdminApi() {
+        return GroupedOpenApi.builder()
+            .group("OURPOS-ADMIN")
+            .packagesToScan("com.ourposapp.api")
+            .pathsToMatch("/api/v1/admin/**")
+            .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+            .components(new Components()
+                .addSecuritySchemes("Authorization",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
